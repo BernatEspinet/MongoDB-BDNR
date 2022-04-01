85aecce42f131ffbe67afe9c5592606de54198e4
SergiCSim <91118120+SergiCSim@users.noreply.github.com>
2022-04-01 10:27:48 +0200

    Actualització codi python

--- Mongodb-BDNR.py
+++ Mongodb-BDNR.py
@@ -7,7 +7,6 @@ mongoUser = ''
 mongoPassword = ''
 mongoDB = ''
 
-# En execució remota
 Host = 'localhost' 
 Port = 27017
 
@@ -19,24 +18,50 @@ bd = conn['MongoDB-BDNR']
 
 try:
     coll_Patient = bd.create_collection('Patient')
-    coll_Nodule= bd.create_collection('Nodule')
-    coll_Method = bd.create_collection('Method')
-    coll_Experiment = bd.create_collection('Experiment')
-except:
+except: 
     bd.Patient.drop()
-    bd.Nodule.drop()
-    bd.Method.drop()
-    bd.Experiment.drop()
     coll_Patient = bd.create_collection('Patient')
+try:
     coll_Nodule= bd.create_collection('Nodule')
-    coll_Method = bd.create_collection('Method')
-    coll_Experiment = bd.create_collection('Experiment')
-    
-csvfile1 = pd.read_excel("C:/Users/Bernat/Downloads/Dades.xlsx", sheet_name="Cases")
-csvfile2 = pd.read_excel("C:/Users/Bernat/Downloads/Dades.xlsx", sheet_name="Training")
-csvfile3 = pd.read_excel("C:/Users/Bernat/Downloads/Dades.xlsx", sheet_name="MethodOutput")
+except:
+    bd.Nodule.drop()
+    coll_Nodule= bd.create_collection('Nodule')
 
-headers_Patient=["PatientID","Age","Gender"]
+try: 
+    coll_Experiment = bd.create_collection('Experiment')
+except:    
+    bd.Experiment.drop()
+    coll_Experiment = bd.create_collection('Experiment')
+try:
+    coll_CTScanner = bd.create_collection('CTScanner')
+except:
+    bd.CTScanner.drop()
+    coll_CTScanner = bd.create_collection('CTScanner')
+
+try:
+    coll_Relation = bd.create_collection('Relation')
+except:
+    bd.Relation.drop()
+    coll_Relation = bd.create_collection('Relation')
+    
+csvfile1 = pd.read_excel("Dades.xlsx", sheet_name="Cases")
+csvfile2 = pd.read_excel("Dades.xlsx", sheet_name="Training")
+csvfile3 = pd.read_excel("Dades.xlsx", sheet_name="MethodOutput")
+
+headers_Relation=["PatientID","NodulID","MethodID", "ExperimentRepetition", "Train", "RadiomicsDiagnosis"]
+col = {}
+for at in headers_Relation:
+    for col_name, data in csvfile2.items():
+        if(col_name in at):
+            for val in data:
+                col[col_name]=data
+    list_of_dicts_Relation = [dict(zip(col,t)) for t in zip(*col.values())]
+
+for dictionary in list_of_dicts_Relation:
+    coll_Relation.insert_one(dictionary)
+
+
+headers_Patient=["PatientID","Age","Gender", "DiagnosisPatient"]
 col = {}
 for at in headers_Patient:
     for col_name, data in csvfile1.items():
@@ -60,18 +85,18 @@ for at in headers_CTScanner:
             for val in data:
                 col[col_name]=data
     list_of_dicts_CTScanner = [dict(zip(col,t)) for t in zip(*col.values())]
+for dictionary in list_of_dicts_CTScanner:
+    coll_CTScanner.insert_one(dictionary)
 
-headers_Experiment=["MethodID","Train","BenignPrec","BenignRec","MalignPrec","MalignRec","Repetition"]
+headers_ScannerEncast=["CTID", "Device", "dataCT","Diameter (mm)"]
 col = {}
-for at in headers_Experiment:
-    for col_name, data in csvfile3.items():
+for at in headers_ScannerEncast:
+    for col_name, data in csvfile1.items():
         if(col_name in at):
             for val in data:
                 col[col_name]=data
-    list_of_dicts_Experiment = [dict(zip(col,t)) for t in zip(*col.values())]
-for dictionary in list_of_dicts_Experiment:
-    coll_Experiment.insert_one(dictionary)
-
+    list_of_dicts_ScannerEncast = [dict(zip(col,t)) for t in zip(*col.values())]
+    
 headers_Nodule=["PatientID","NoduleID","DiagnosisPatient","DiagnosisNodule","PositionX","PositionY","PositionZ"]
 col = {}
 for at in headers_Nodule:
@@ -80,9 +105,8 @@ for at in headers_Nodule:
             for val in data:
                 col[col_name]=data
     list_of_dicts = [dict(zip(col,t)) for t in zip(*col.values())]
-for dictionary1,dictionary2 in zip(list_of_dicts,list_of_dicts_CTScanner):
+for dictionary1,dictionary2 in zip(list_of_dicts,list_of_dicts_ScannerEncast):
     dictionary1["CTScanner"]=dictionary2
-    #dictionary1["Experiment"]=dictionary3
     coll_Nodule.insert_one(dictionary1)
 
 
@@ -93,12 +117,25 @@ for at in headers_Method:
         if(col_name in at):
             for val in data:
                 col[col_name]=data
-    list_of_dicts = [dict(zip(col,t)) for t in zip(*col.values())]
-id_added=[]
-for dictionary in list_of_dicts:
-    dictionary["_id"]=dictionary["MethodID"]
-    dictionary.pop("MethodID")
-    if(dictionary["_id"] not in id_added):
-        coll_Method.insert_one(dictionary)
-        id_added.append(dictionary["_id"])
-        
+    list_of_dicts_Method = [dict(zip(col,t)) for t in zip(*col.values())]
+headers_Experiment=["Train","BenignPrec","BenignRec","MalignPrec","MalignRec","Repetition"]
+col = {}
+for at in headers_Experiment:
+    for col_name, data in csvfile3.items():
+        if(col_name in at):
+            for val in data:
+                col[col_name]=data
+    list_of_dicts_Experiment = [dict(zip(col,t)) for t in zip(*col.values())]
+headers_NoduleEncast=["NoduleID","DiagnosisNodule"]
+col = {}
+for at in headers_NoduleEncast:
+    for col_name, data in csvfile1.items():
+        if(col_name in at):
+            for val in data:
+                col[col_name]=data
+    list_of_dicts_NoduleEncast = [dict(zip(col,t)) for t in zip(*col.values())]
+for dictionary1,dictionary2, dictionary3 in zip(list_of_dicts_Experiment,list_of_dicts_Method, list_of_dicts_NoduleEncast):
+    dictionary1["Method"]=dictionary2
+    dictionary1["Nodule"]=dictionary3
+    coll_Experiment.insert_one(dictionary1)
+
